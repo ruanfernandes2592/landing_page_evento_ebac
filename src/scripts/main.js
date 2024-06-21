a6066ebc118d928d072f0d0f90bce8f8ccad1aa6
ruanfernandes2592 <ruanfernandes2592@gmail.com>
2024-06-21 12:47:55 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('Olá mundo!');
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampDoAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampDoAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    const segundoEmMs = 1000;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / segundoEmMs);
+
+    document.querySelector('#contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.querySelector('#contador').innerHTML = `Evento expirado`
+    }
+}, 1000);
